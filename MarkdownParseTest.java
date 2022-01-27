60af0c1bac3fab731829563305e1ef1972490931
CJWAKE998 <97641362+CJWAKE998@users.noreply.github.com>
2022-01-26 16:38:15 -0800

    Added new tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,4 +1,8 @@
 import static org.junit.Assert.*;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Path;
 import org.junit.*;
 
 public class MarkdownParseTest {
@@ -6,4 +10,16 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    ArrayList<String> list1 = new ArrayList<>();
+
+    @Test
+    public void getLinksTest() throws IOException{
+        Path nameOfFile = Path.of("test-file.md");
+        String  fileContents = Files.readString(nameOfFile);
+        list1.add("https://something.com");
+        list1.add("some-page.html");
+        assertEquals("This should work", list1 , MarkdownParse.getLinks(fileContents));
+    }
 }
+
