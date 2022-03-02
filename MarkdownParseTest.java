bdb32ca18aeb82cb158c4620285e1f7db29131d1
CJWAKE998 <97641362+CJWAKE998@users.noreply.github.com>
2022-03-01 22:57:37 -0800

    didn't change anything

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,104 +1,94 @@
-import static org.junit.Assert.*;
-import java.nio.file.*;
-import java.util.ArrayList;
+// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import org.junit.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class MarkdownParseTest {
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+public class MarkdownParse {
+
+    static int findCloseParen(String markdown, int openParen) {
+        int closeParen = openParen + 1;
+        int openParenCount = 1;
+        while (openParenCount > 0 && closeParen < markdown.length()) {
+            if (markdown.charAt(closeParen) == '(') {
+                openParenCount++;
+            } else if (markdown.charAt(closeParen) == ')') {
+                openParenCount--;
+            }
+            closeParen++;
+        }
+        if(openParenCount == 0) {
+          return closeParen - 1;
+        }
+        else {
+          return -1;
+        }
+
     }
-
-    
-
-    @Test
-    public void getLinksTest0() throws IOException{
-        ArrayList<String> list1 = new ArrayList<>();
-        Path nameOfFile = Path.of("break0.md");
-        String  fileContents = Files.readString(nameOfFile);
-        list1.add("https://something.com");
-        list1.add("some-page.html");
-        assertEquals("This should work", list1 , MarkdownParse.getLinks(fileContents));
+    public static Map<String, List<String>> getLinks(File dirOrFile) throws IOException {
+        Map<String, List<String>> result = new HashMap<>();
+        int counter = 0;
+        if(dirOrFile.isDirectory()) {
+            for(File f: dirOrFile.listFiles()) {
+                counter++;
+                result.putAll(getLinks(f));
+            }
+            System.out.println(counter);
+            return result;
+        }
+        else {
+            Path p = dirOrFile.toPath();
+            int lastDot = p.toString().lastIndexOf(".");
+            if(lastDot == -1 || !p.toString().substring(lastDot).equals(".md")) {
+                return result;
+            }
+            ArrayList<String> links = getLinks(Files.readString(p));
+            result.put(dirOrFile.getPath(), links);
+            return result;
+        }
     }
+    public static ArrayList<String> getLinks(String markdown) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        // find the next [, then find the ], then find the (, then take up to
+        // the next )
+        int currentIndex = 0;
+        while(currentIndex < markdown.length()) {
+            int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            int nextCodeBlock = markdown.indexOf("\n```");
+            if(nextCodeBlock < nextOpenBracket && nextCodeBlock != -1) {
+                int endOfCodeBlock = markdown.indexOf("\n```");
+                currentIndex = endOfCodeBlock + 1;
+                continue;
+            }
+            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+            int openParen = markdown.indexOf("(", nextCloseBracket);
 
-    @Test
-    public void getLinksTest1() throws IOException{
-        ArrayList<String> list1 = new ArrayList<>();
-        Path nameOfFile = Path.of("break1.md");
-        String  fileContents = Files.readString(nameOfFile);
-        list1.add("https://something.com");
-        list1.add("some-page.html");
-        assertEquals("This should work", list1 , MarkdownParse.getLinks(fileContents));
+            // The close paren we need may not be the next one in the file
+            int closeParen = findCloseParen(markdown, openParen);
+            
+            if(nextOpenBracket == -1 || nextCloseBracket == -1
+                  || closeParen == -1 || openParen == -1) {
+                return toReturn;
+            }
+            String potentialLink = markdown.substring(openParen + 1, closeParen).trim();
+            if(potentialLink.indexOf(" ") == -1 && potentialLink.indexOf("\n") == -1) {
+                toReturn.add(potentialLink);
+                currentIndex = closeParen + 1;
+            }
+            else {
+                currentIndex = currentIndex + 1;
+            }
+        }
+        return toReturn;
     }
-
-    @Test
-    public void getLinksTest2() throws IOException{
-        ArrayList<String> list1 = new ArrayList<>();
-        Path nameOfFile = Path.of("break2.md");
-        String  fileContents = Files.readString(nameOfFile);
-        list1.add("https://something().com");
-        assertEquals("This should work", list1 , MarkdownParse.getLinks(fileContents));
+    public static void main(String[] args) throws IOException {
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = getLinks(contents);
+        System.out.println(links);
     }
-
-    @Test
-    public void getLinksTest3() throws IOException{
-        ArrayList<String> list1 = new ArrayList<>();
-        Path nameOfFile = Path.of("break3.md");
-        String  fileContents = Files.readString(nameOfFile);
-        assertEquals("This should work", list1 , MarkdownParse.getLinks(fileContents));
-    }
-
-
-    @Test
-    public void getLinksTestSnip1() throws IOException{
-        ArrayList<String> list1 = new ArrayList<>();
-        Path nameOfFile = Path.of("Snippet1.md");
-        String  fileContents = Files.readString(nameOfFile);
-        list1.add("url.com");
-        list1.add("`google.com");
-        list1.add("google.com");
-        list1.add("ucsd.edu");
-        assertEquals("This should work", list1 , MarkdownParse.getLinks(fileContents));
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    /*@Test
-    public void getLinksTestSnip2() throws IOException{
-        ArrayList<String> list1 = new ArrayList<>();
-        Path nameOfFile = Path.of("Snippet2.md");
-        String  fileContents = Files.readString(nameOfFile);
-        list1.add("b.com");
-        list1.add("a.com(())");
-        list1.add("example.com");
-        assertEquals("This should work", list1 , MarkdownParse.getLinks(fileContents));
-    }
-
-    @Test
-    public void getLinksTestSnip3() throws IOException{
-        ArrayList<String> list1 = new ArrayList<>();
-        Path nameOfFile = Path.of("Snippet3.md");
-        String  fileContents = Files.readString(nameOfFile);
-        list1.add("https://www.twitter.com");
-        list1.add("https://ucsd-cse15l-w22.github.io/");
-        list1.add("github.com");
-        list1.add("https://cse.ucsd.edu/");
-        assertEquals("This should work", list1 , MarkdownParse.getLinks(fileContents));
-    }
-*/
-    
 }
-
